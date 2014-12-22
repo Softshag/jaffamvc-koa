a07425d8e35eea272aee7b0789526a7fc434e473
Rasmus Kildevæld <admin@softshag.dk>
2014-12-22 13:33:08 +0100

    initize.js now uses generators.

--- example/initializers/initialize.js
+++ example/initializers/initialize.js
@@ -1,11 +1,11 @@
 
 var Promise = require('bluebird');
 
-module.exports = function * () {
+module.exports = function () {
 
   return Promise.delay(2000).then(function () {
     console.log('tjopper');
 
-    
+
   });
 };
