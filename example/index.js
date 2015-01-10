64dac40e17e02536c0468f38ce77f936a2e8dbaa
Rasmus Kildevæld <admin@softshag.dk>
2015-01-10 15:02:07 +0100

    Application is now an event emitter. Added dispatch error

--- example/index.js
+++ example/index.js
@@ -6,8 +6,11 @@ jaffamvc = require('../lib');
 
 app = jaffamvc();
 
+app.onAny(function () {
+  console.log('all',this.event)
+})
 app.default().start().then(function () {
   this.listen(3000);
 }).catch(function (err) {
-  console.log(err);
+  console.log(err.stack);
 });
