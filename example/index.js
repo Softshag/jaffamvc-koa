4ab7ceb856e4505945a6e20d1e957950fef9d2d6
Rasmus Kildevæld <admin@softshag.dk>
2015-01-10 15:19:43 +0100

    check for path existence in initializers.

--- example/index.js
+++ example/index.js
@@ -6,9 +6,7 @@ jaffamvc = require('../lib');
 
 app = jaffamvc();
 
-app.onAny(function () {
-  console.log('all',this.event)
-})
+
 app.default().start().then(function () {
   this.listen(3000);
 }).catch(function (err) {
