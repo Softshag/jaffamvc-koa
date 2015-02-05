57028c1fcdafa2e6e966d61eb7fb2ad71e586b25
Rasmus Kildevæld <admin@softshag.dk>
2015-02-05 20:57:10 +0100

    added new events to router

--- example/index.js
+++ example/index.js
@@ -1,13 +1,27 @@
 
 var profiler = require('../lib/middlewares/profiler');
+var logger = require('../lib/middlewares/logger.js');
+var rpc = require('../modules/rpc');
 var jaffamvc, app;
 
 jaffamvc = require('../lib');
 
 app = jaffamvc();
 
-app.use(profiler());
 
+app.phase('some', function () {
+  console.log('some phase');
+
+});
+
+app.phase('rpc',rpc);
+
+app.use(profiler());
+app.use(logger());
+
+app.on('run', function (task) {
+  console.log('task', task)
+})
 
 app.start().then(function () {
   console.log('started')
