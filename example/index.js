2abd2f3424e2315cb920e65d4aabf454a8b83ea9
Rasmus Kildevæld <admin@softshag.dk>
2015-09-09 13:28:34 +0200

    updated example

--- example/index.js
+++ example/index.js
@@ -8,8 +8,8 @@ let logger = require('../lib/middlewares/logger.js'),
 
 let app = new JaffaMVC();
 
-app.use(logger());
-app.use(profiler());
+//app.use(logger());
+//app.use(profiler());
 const PORT = process.env.PORT || 3000;
 
 app.start(PORT).then(function () {
